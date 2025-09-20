d1a1520edb0703fbd0dcc66cc93e30b1fa0fb71b
beeper9-source <beeper9@gmail.com>
2025-09-21 01:46:50 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/AQUA/', // GitHub Pages 경로에 맞게 설정
   server: {
     port: 3000,
     host: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 })
-
-
